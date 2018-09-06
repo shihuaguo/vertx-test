6443d781f8a88f1c5bc601f01eadf578a193aab4
shihuaguo <shihuaguo@mindai.com>
2018-09-06 20:35:22 +0800

    add httpserver verticle

---
+++ src/main/java/com/mindai/vertx/http/HttpServerVerticle.java
@@ -0,0 +1,45 @@
+package com.mindai.vertx.http;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+
+public class HttpServerVerticle extends AbstractVerticle {
+
+	@Override
+	public void start(Future<Void> startFuture) throws Exception {
+		HttpServer server = vertx.createHttpServer();
+		server.requestHandler(req -> {
+			System.out.println("http request receive:");
+			System.out.println("version: " + req.version());
+			System.out.println("method: " + req.method());
+			System.out.println("uri: " + req.uri());
+			if(req.method() == HttpMethod.POST) {
+				req.bodyHandler(buffer -> {
+					System.out.println("receive body: " + buffer.toString());
+				});
+			}
+			req.response().end("hello,world");
+		});
+		server.listen(8001, "localhost", res -> {
+			if(res.succeeded()) {
+				System.out.println("httpserver listen on 8001 success");
+			}else {
+				System.err.println("httpserver listen on 8001 failed");
+			}
+		});
+		super.start(startFuture);
+	}
+
+	@Override
+	public void stop(Future<Void> stopFuture) throws Exception {
+		super.stop(stopFuture);
+	}
+	
+	public static void main(String[] args) {
+		Vertx.vertx().deployVerticle(new HttpServerVerticle());
+	}
+
+}
